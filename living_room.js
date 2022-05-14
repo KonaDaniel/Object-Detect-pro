4b8120ab6853c87d97626c4b3718dd7799209a58
KonaDaniel <79358753+KonaDaniel@users.noreply.github.com>
2022-05-14 17:03:19 +0530

    Add files via upload

---
+++ living_room.js
@@ -0,0 +1,50 @@
+status = "";
+objects = [];
+
+function setup()
+{
+    canvas = createCanvas(650, 420);
+    canvas.center();
+    objectDetection = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function preload()
+{
+    img = loadImage('livingroom.jpg');
+}
+
+function modelLoaded()
+{
+    status = true;
+    console.log("Cocossd Loaded!")
+    objectDetection.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw()
+{
+    image(img, 0, 0, 650, 420);
+
+    if(status != "")
+    {
+       for(i = 0; i < objects.length; i++)
+       {
+        document.getElementById("status").innerHTML = "Status : Objects being detected";
+        fill("red");
+        stroke("red");
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+        noFill();
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+       } 
+    }
+}
